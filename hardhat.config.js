8dd8e0c5daf351a25c0779170adc57108b9ccba8
gilbmp <gilbmp@gmail.com>
2022-10-29 11:02:50 -0300

    The Raffle smart contract made so far

---
+++ hardhat.config.js
@@ -0,0 +1,55 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config()
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "api-key"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  solidity: {
+    compilers: [{ version: "0.8.7" }, { version: "0.6.6" }],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+    goerli: {
+      default: 1,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+}
